5db82efbe1d0c031a3cd0bb9adee680c438d2ed3
harshavardhan2601 <74228616+harshavardhan2601@users.noreply.github.com>
2020-11-28 14:35:39 +0530

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { SignaturePadComponent } from './signature-pad/signature-pad.component';
+
+const routes: Routes = [
+  {path: '', component: SignaturePadComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
